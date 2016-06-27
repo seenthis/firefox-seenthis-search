b3869e2f804ca8749f8d422dda87b3f330d6782e
brunob <brunobergot@gmail.com>
2016-06-27 16:27:02 +0200

    cleanup

--- background.js
+++ background.js
@@ -11,28 +11,3 @@ chrome.commands.onCommand.addListener(function(command) {
 });
 
 chrome.browserAction.onClicked.addListener(openView);
-
-/*
-function openIncSearch(aWindow) {
-  var gBrowser = aWindow.gBrowser;
-
-  var openUrl = 'chrome://seenthis_search/content/view.html';
-  var target = null;
-
-  var tabs = gBrowser.tabContainer.childNodes;
-
-  for (var i = 0, len = tabs.length; i < len; i++) {
-    if (tabs[i].linkedBrowser.currentURI.spec == openUrl) {
-      target = tabs[i];
-      break;
-    }
-  }
-
-  if (!target) {
-    gBrowser.selectedTab = gBrowser.addTab(openUrl);
-  } else {
-    gBrowser.selectedTab = target;
-    target.linkedBrowser.contentDocument.getElementById('text').focus();
-  }
-}
-*/
